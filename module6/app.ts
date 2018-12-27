efed977e82d481506380070a5dfbf948a1db96a1
albert18 <caps18@hotmail.com>
2018-12-27 17:15:36 -0500

    update

--- module6/app.ts
+++ module6/app.ts
@@ -1,15 +1,2 @@
-namespace MyMath {
-    const PI = 3.14;
-
-    export function calculateCircumference(diameter: number) {
-    return diameter * PI;
-    }
-
-    export function calculateRectangle(width: number, length: number) {
-    return width * length;
-    }
-
-}
-
 
 console.log(MyMath.calculateRectangle(10,20))
