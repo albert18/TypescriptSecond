40bec986ff98ab3aef9740a071e3e599626a5ac2
albert18 <caps18@hotmail.com>
2019-01-01 13:14:30 -0500

    interface and properties

--- module7/app.js
+++ module7/app.js
@@ -1,2 +1,14 @@
 "use strict";
-alert("test");
+function greet(person) {
+    console.log("Hello, " + person.firstName);
+}
+function changeName(person) {
+    person.firstName = "Anna";
+}
+var person = {
+    firstName: "Max",
+    age: 27
+};
+greet(person);
+changeName(person);
+greet(person);
--- module7/app.ts
+++ module7/app.ts
@@ -1,1 +1,20 @@
-alert("test");
+interface NamedPerson {
+    firstName: string;
+}
+
+function greet(person: NamedPerson) {
+    console.log("Hello, " + person.firstName);
+}
+
+function changeName(person: NamedPerson) {
+    person.firstName = "Anna"
+}
+
+const person = {
+    firstName: "Max",
+    age: 27
+}
+
+greet(person)
+changeName(person);
+greet(person)
