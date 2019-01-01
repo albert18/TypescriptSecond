7885817245738d88268172bc68121223b2df439f
albert18 <caps18@hotmail.com>
2019-01-01 16:31:13 -0500

    Generic Types

--- mod8/app.ts
+++ mod8/app.ts
@@ -7,11 +7,39 @@ function echo(data: any) {
 // console.log(echo(27));
 // console.log(echo({name: "Max1", age: 27}));
 
+
+// ---------------------------------------------------------
 /// Better Generic way -- its IDE support 
 function betterEcho<T>(data: T) {
     return data;
 }
 
-console.log(betterEcho("Max").length);
-console.log(betterEcho<number>(27)); // specificly
-console.log(betterEcho({name: "Max1", age: 27}));
+// console.log(betterEcho("Max").length);
+// console.log(betterEcho<number>(27)); // specificly
+// console.log(betterEcho({name: "Max1", age: 27}));
+
+
+// ---------------------------------------------------------
+//Build in Genetics - Array is a generic type by default
+const testResults:Array<number> = [1.93, 2.33];
+testResults.push(2.99);
+// console.log(testResults);
+
+// ---------------------------------------------------------
+// Arrays
+function printAll<T>(args:T[]) {
+    args.forEach((element) => console.log(element));
+}
+
+// printAll(["Apple", "Banana", "Orange"]);
+
+
+
+// ---------------------------------------------------------
+// Generics Types
+const echo2: <T>(data: T) => T = echo;
+// console.log(echo2<string>("Something"));
+
+
+// ---------------------------------------------------------
+// Creating a Generic Class
