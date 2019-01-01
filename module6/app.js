df434b4395266e3dd9ca6e8d1ec47b026c90124b
albert18 <caps18@hotmail.com>
2019-01-01 11:58:57 -0500

    loading modules

--- module6/app.js
+++ module6/app.js
@@ -1,18 +1,4 @@
-var MyMath;
-(function (MyMath) {
-    var PI = 3.14;
-    function calculateCircumference(diameter) {
-        return diameter * PI;
-    }
-    MyMath.calculateCircumference = calculateCircumference;
-})(MyMath || (MyMath = {}));
-var MyMath;
-(function (MyMath) {
-    function calculateRectangle(width, length) {
-        return width * length;
-    }
-    MyMath.calculateRectangle = calculateRectangle;
-})(MyMath || (MyMath = {}));
+"use strict";
 /// <reference path="circleMath.ts"/>
 /// <reference path="rectangleMath.ts"/>
 console.log(MyMath.calculateRectangle(10, 20));
---
+++ module6/appMath.ts
@@ -0,0 +1,6 @@
+import {PI, calculateCirumcumference} from "./math/circle"
+
+
+console.log(PI);
+console.log(calculateCirumcumference(10));
+console.log();
