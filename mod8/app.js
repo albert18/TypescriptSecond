5943e82eaece06015c9d8a30a7bc0473d4adc290
albert18 <caps18@hotmail.com>
2019-01-01 16:13:36 -0500

    creating generic function

--- mod8/app.js
+++ mod8/app.js
@@ -1,2 +1,15 @@
 "use strict";
-alert("test1231");
+// Simple Generic
+function echo(data) {
+    return data;
+}
+// console.log(echo("Max"));
+// console.log(echo(27));
+// console.log(echo({name: "Max1", age: 27}));
+/// Better Generic way -- its IDE support 
+function betterEcho(data) {
+    return data;
+}
+console.log(betterEcho("Max").length);
+console.log(betterEcho(27)); // specificly
+console.log(betterEcho({ name: "Max1", age: 27 }));
--- mod8/app.ts
+++ mod8/app.ts
@@ -1,1 +1,17 @@
-alert("test1231");
+// Simple Generic
+function echo(data: any) {
+    return data;
+}
+
+// console.log(echo("Max"));
+// console.log(echo(27));
+// console.log(echo({name: "Max1", age: 27}));
+
+/// Better Generic way -- its IDE support 
+function betterEcho<T>(data: T) {
+    return data;
+}
+
+console.log(betterEcho("Max").length);
+console.log(betterEcho<number>(27)); // specificly
+console.log(betterEcho({name: "Max1", age: 27}));
