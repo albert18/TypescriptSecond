1cc9da048e4f2004e26760ccbba89583fd0ec8ea
albert18 <caps18@hotmail.com>
2019-01-03 07:41:14 -0500

    creating a class decorator

--- 9/9.ts
+++ 9/9.ts
@@ -1,1 +1,12 @@
-alert("Test");
+// A decorator is just a function / appending by the @ sign
+// Note to remove the warning need to enable/add experimentalDecorators in tsconfig
+function logged(constructorFN: Function) {
+    console.log(constructorFN);
+}
+
+@logged
+class Person {
+    constructor() {
+        console.log("Hi!")
+    }
+}
